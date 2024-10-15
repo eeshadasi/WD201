d3c79d968e74bbc30f1c74d6a2e2f9ae3ca0510e
Eesha Dasi <dasideesha@gmail.com>
2024-10-15 22:02:44 +0530

    some text

--- http-server/index.js
+++ http-server/index.js
@@ -1,15 +1,45 @@
-const http=require('http')
-const fs=require('fs')
+const http=require('http');
+const fs=require('fs');
+let projectContent="";
+let homeContent="";
+let registrationContent="";
 fs.readFile("home.html",(error,home)=>{
     if (error){
         throw error;
     }
-    http
-    .createServer((request,response)=>{
-        response.writeHeader(200,{"Content-Type":"text/HTML"});
-        response.write(home);
-        response.end(); 
-    })
-    .listen(3000);
+    homeContent=home;
+});
+fs.readFile("project.html",(error,project)=>{
+    if (error){
+        throw error;
+
+    }
+    projectContent=project;
 
 });
+fs.readFile("registration.html",(error,registration)=>{
+    if (error){
+        throw error;
+    }
+    registrationContent=registration;
+});
+http
+    .createServer((request,response)=>{
+        url=request.url;
+        response.writeHeader(200,{content:"text/html"});
+        switch(url){
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            case "/registration.html":
+                response.write(registrationContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+            }
+        })
+    .listen(3000);
